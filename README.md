8afcb2692e359bfdddf28a866a739f7014fe3a6c
Abby E <saegbo@yahoo.com>
2023-01-23 22:08:06 +0000

    Appointments saved in localstorage

---
+++ README.md
@@ -0,0 +1,5 @@
+Calender-Application Project
+
+The propject involves creating  a simple calendar application that allows users to save events for each hour of the day. 
+
+The app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
--- Script.js
+++ Script.js
@@ -1,16 +1,48 @@
+
+  let $currentTextarea = $("textarea[data-time='09am']")
+
+    console.log ($currentTextarea)
+
 // display current time on the screen
 
-let today= moment();
+// time updates every  second
 
-$("#currentDay").text(today.format ("dddd D MMM YYYY"));
+setInterval(
+function(){
+    $("#currentDay").text(moment().format ("DDD MMM YYYY hh:mm:ss"));
+},1000);
+
 
 // Creat a button click function
 
 
-$("button").on("click", function(event){
+  $("button").on("click", function(event){
 
+  
+    localStorage.setItem(event.target.innerText, JSON.stringify(event.target.previousElementSibling.value))
+    console.log(event.target.innerText)
 
-})
+      // compare to current time is 9am before of after the current moment
+      
 
-// compare to current time is 9am before of after the current moment
+    //   if(moment().format("h") > $currentTextarea.attr("data-time")){
+          
+    //       console.log("hello");
+     
+    //     }
+        
+
+ })
+
+//  this retrieves appts from local storage to populates the time blocks
+function GetAppointment(){
+
+// localStorage.getItem ("11aam")
+
+console.log(JSON.parse(localStorage.getItem("11am")))
+
+}
+
+GetAppointment()
+
 // if it then we want to apply task to it. if current time is 12pm and we are on the 12pm textarea. 
